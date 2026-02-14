5059c810c43cee9014846be0b3c282ec45d69863
Faridum <farid.jamaleldin@gmail.com>
2026-02-14 21:48:57 +0200

    Add GitHub Actions CI

--- src/main/java/com/zad/automation/driver/DriverFactory.java
+++ src/main/java/com/zad/automation/driver/DriverFactory.java
@@ -3,16 +3,26 @@ package com.zad.automation.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     public static WebDriver initDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
 
         WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         return driver;
+
+
+
     }
 }
